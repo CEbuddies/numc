07c132832320dc549aca1fe15825e2ac5f5b3dba
nic <nic9323@gmail.com>
2025-04-22 08:59:38 +0200

    Tests for arbitrary types

--- NumC.h
+++ NumC.h
@@ -2,24 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #ifndef NUMC_H
 #define NUMC_H
-// TODO: for the moment just integers
-// TODO: Can alter have something like free all or similar, but would need tracker then
-//
 
-
-// not easy to make this generic
-// can later become a union or something special where string is parsed
-/*
-typedef struct {
-    int shape0;
-    int shape1;
-    int shape2;
-    int shape3;
-} Shape;
-*/
 typedef union {
     struct {
         int s0;
@@ -40,72 +27,59 @@ typedef enum {
 } Type;
 
 typedef struct {
-    int * array;
-    int len; // TODO: should be shape or something
-    int shape[5]; // experimental, but is this good?
+    void * array;
+    int len;
+    Type type;
 } Array;
 
 typedef struct {
-    double * array;
-    int len; // TODO: should be shape or something
-    int shape[5]; // experimental, but is this good?
-} ArrayD;
+    int8_t * array;
+    int len;
+    Type type;
+} ArrayI;
+
+typedef struct {
+    int8_t * array;
+    int len;
+    Type type;
+} ArrayI8;
 
 typedef struct {
     float * array;
-    int len; // TODO: should be shape or something
-    int shape[5]; // experimental, but is this good?
+    int len;
+    Type type;
 } ArrayF;
 
 typedef struct {
+    void * array;
+    int len;
+    Type type;
+} ArrayG;
+
+typedef struct {
+	ArrayI (*zeros)(int len);
+	void (*fill)(ArrayI array, int8_t val);
+} Int;
+
+typedef struct {
+	ArrayF (*zeros)(int len);
+	ArrayF (*fill)(ArrayF array, float val);
+} Float;
+
+typedef struct {
     Array (*randint)(int len);
-    ArrayF (*ranF)(int len);
-    ArrayD (*ranD)(int len);
+    ArrayI (*zeros)(int len); 
+    ArrayF (*zerosf)(int len);
     int (*max)(Array array); // should take any array
     int (*min)(Array array);
     double (*scalar)(Array a1, Array a2);
-    int ptr_counter;
-    int len_ptr_list;
-    int ** ptr_list;
+    Int I;
+    Float Fl;
 } NumC;
 
 
 NumC numcinit();
-int clean_up(NumC * nc);
-void add_ref(NumC * nc, Array * array);
-
-static Array make_Array(int * array, int len){
-    Array newArray =
-    {
-        .array = array, 
-        .len = len // TODO: needs to work with shape somehow
-    };
-
-    return newArray;
-}
-
-// takes in a string of shape (n, m, k)
-void shape_str(char * shapestr){
-}
-
-size_t calc_bytes(Shape sh, int dims, Type t){
-    int bytes;
-    switch(t){
-        case INT:
-            bytes = 4;
-            break;
-        case FLT:
-            bytes = 4;
-            break;
-        case DBL:
-            bytes = 8;
-            break;
-    }
-    
-    
-
-}
-
+/*
 double std_scalar(Array a1, Array a2){
     // here goes checking
     // TODO: do proper error handling here
@@ -119,7 +93,7 @@ double std_scalar(Array a1, Array a2){
     }
     return sum;
 }
-
+*/
 Array rint_(int len){
     int * array = calloc(len, sizeof(int));
     srand(time(NULL));
@@ -127,113 +101,97 @@ Array rint_(int len){
     for (int i=0; i<len; i++){
         array[i] = rand();
     }
-    Array r_array = make_Array(array, len);
-    // would like to make this a function but causes pointer troubles
+    // fill the array
+    Array r_array;
+    r_array.array = array;
+    r_array.len = len;
+    r_array.type = INT;
     
     return r_array;
 }
-
-ArrayD _zeros(Shape sh){
-    int dimension = 1;
-    if (sh.shape1 > 0){
-        dimension = 2;
-    }
-    if (sh.shape2 > 0){
-        dimension = 3;
-    }
-    if (sh.shape3 > 0){
-        dimension = 4;
-    }
-
-    return r_a;
-}
-
-ArrayD ranD(int len){
-    double * array = calloc(len, sizeof(double));
-    srand(time(NULL));
-    for (int i=0; i<len; i++){
-        array[i] = (double)rand()/RAND_MAX;
-    }
-    ArrayD r_a = 
-    {
-        .array = array,
-        .len = len
-    };
-    return r_a;
-}
-
-ArrayF ranF(int len){
-    float * array = calloc(len, sizeof(float));
-    srand(time(NULL));
-    for (int i=0; i<len; i++){
-        array[i] = (float)rand()/RAND_MAX;
-    }
-    ArrayF r_a = 
-    {
-        .array = array,
-        .len = len
-    };
-    return r_a;
-}
-
-int maxint(Array array){
+// TODO: might need typecast or something
+int __maxint(int * array, int len){
     // how to go about overflows?
-    int max = array.array[0];
-    for (int i=0; i<array.len; i++){
-        if (array.array[i] > max){
-            max = array.array[i];
+    int max = array[0];
+    for (int i=0; i<len; i++){
+        if (array[i] > max){
+            max = array[i];
         }
     }
     return max;
 }
 
-int minint(Array array){
-    int min = array.array[0];
-    for (int i=0; i<array.len; i++){
-        if (array.array[i] < min){
-            min = array.array[i];
+int __minint(int * array, int len){
+    int min = array[0];
+    for (int i=0; i<len; i++){
+        if (array[i] < min){
+            min = array[i];
         }
     }
     return min;
 }
 
-int * add_array(Array * array){
-    return array->array;
+ArrayI __zeros(int len){
+	ArrayI array;
+	array.len = len;
+	array.type = INT;
+	array.array = calloc(len, sizeof(int8_t));
+
+	return array;
+}
+
+ArrayI8 __zerosG(int len){
+	ArrayI8 array;
+	array.len = len;
+	array.type = INT;
+	array.array = calloc(len, sizeof(int8_t));
+}
+
+ArrayF __zerosf(int len){
+	ArrayF array;
+	array.len = len;
+	array.type = FLT;
+	array.array = calloc(len, sizeof(float));
+
+	return array;
+}
+void __fill(ArrayI array, int8_t val){
+	for (int i=0; i<array.len; i++) {
+		array.array[i] = val;
+	}
+}
+
+// Fill function for floats
+void __fillf(ArrayF array, float val){
+	for (int i=0; i<array.len; i++) {
+		array.array[i] = val;
+	}
+}
+
+double __isum(ArrayI array) {
+	double sum = 0;
+	for (int i=0; i<array.len; i++) {
+		sum += array.array[i];
+	}
+	return sum;
 }
 
 // how to clear this later?
 NumC numcinit(){
 
     NumC nc;
-    nc.max = &maxint;
-    nc.min = &minint;
+    // nc.max = &__maxint;
+    // nc.min = &__minint;
+    nc.zeros = &__zeros;
+    nc.zerosG = &__zerosG;
+    nc.zerosf = &__zerosf;
     nc.randint = &rint_;
-    nc.scalar = &std_scalar;
-    nc.ranD = &ranD;
-    nc.ptr_list = calloc(10, sizeof(int*)); // FIXME: This must be variable
-    nc.ptr_counter= 0;
-    nc.len_ptr_list = 10;
+    nc.I.zeros = &__zeros;
+    nc.I.fill = &__fill;
+    nc.Fl.zeros = &__zerosf;
+    nc.Fl.fill = &__fillf;
+    // nc.scalar = &std_scalar;
     return nc;
 }
 
-static void update_pointer_count(NumC * nc){
-    if (nc->ptr_counter + 1 >= nc->len_ptr_list){
-        nc->ptr_list = realloc(nc->ptr_list, 2 * nc->len_ptr_list * sizeof(int*));
-        nc->len_ptr_list = 2 * nc->len_ptr_list;
-    }
-    nc->ptr_counter += 1; // what to do if realloc fails?
-}
-
-// Add array to ref counter - not too bad if forgotten
-void add_ref(NumC * nc, Array * array){
-    update_pointer_count(nc);
-    nc->ptr_list[nc->ptr_counter] = array->array;
-}
-
-int clean_up(NumC * nc){
-    for (int i=0; i< nc->ptr_counter; i++){
-        free(nc->ptr_list[i]);
-    }
-    return 1; // when do we alter this?
-}
 #endif
--- main.c
+++ main.c
@@ -26,36 +26,12 @@ int main(){
     printf("NumC testsuite!\n");
     NumC nc = numcinit(); // "import" numpy
     const int TEST_ARR_LEN = 5;
+    ArrayI Iempty = nc.I.zeros(TEST_ARR_LEN);
+    ArrayF Fempty = nc.Fl.zeros(TEST_ARR_LEN);
+    nc.I.fill(Iempty, 3);
 
-    Array r_a = nc.randint(TEST_ARR_LEN);
-    Array r_a2 = nc.randint(TEST_ARR_LEN);
-    ArrayD a_d = nc.ranD(23);
-    r_a2.array[0] = -120;
-    // This can be done nicer
-    add_ref(&nc, &r_a); 
-    //for (int i=0; i<TEST_ARR_LEN; i++){
-    //    printf("El %d: %d\n", i, r_a.array[i]);
-    //}
+    printf("Checking, value number 3 from empty array: %i\n", Iempty.array[3]);
 
-    printf("Max is: %d\n", nc.max(r_a));
-    printf("Min is: %d\n", nc.min(r_a));
-    printf("Scalar product: %lf\n", nc.scalar(r_a2, r_a));
-
-    printf("Parsing ... \n");
-    int dim0, dim1, dim2;
-    sscanf("(23, 4,6)", "(%d, %d, %d)", &dim0, &dim1, &dim2);
-
-    printf("Loop through string ...\n");
-    char somestr[] = "123";
-    parse_matstring(somestr);
-    printf("Dimensions: %d, %d, %d\n", dim0, dim1, dim2);
-    SomeStruct ss = {
-        .a = 4,
-        .b = 5
-    };
-
-    // void pointer array returning
-    clean_up(&nc); 
     return 0;
 }
 
