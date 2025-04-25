9f65611c7910621aa6c667ea0b9f694bc5d521e0
nic <nic9323@gmail.com>
2025-04-25 22:14:02 +0200

    Updated main to new XArray style

--- main.c
+++ main.c
@@ -17,20 +17,7 @@ int main(){
     printf("NumC testsuite!\n");
     NumC nc = numcinit(); // "import" numpy
     const int TEST_ARR_LEN = 7;
-    ArrayI8 emptyi8 = nc.I8.zeros(TEST_ARR_LEN);
-    nc.I8.fill(emptyi8, 4);
-
-    size_t count = nc.I8.count(emptyi8, 4);
-    printf("Count of 4 in i8 array: %ld\n", count);
-    double scalar = nc.scalar(emptyi8.array, emptyi8.array, emptyi8.len, emptyi8.type);
-    printf("Standard scalar product is: %lf\n", scalar);
-    emptyi8.array[3] = 6;
-    emptyi8.array[1] = 6;
-    ArrayI ari = nc.I.zeros(TEST_ARR_LEN);
-    ArrayI locs = nc.I8.whereAll(emptyi8, 6);
-    double max = nc.max(emptyi8.array, emptyi8.len, emptyi8.type);
-    printf("Len of locs: %i\n", locs.len);
-    printf("Max found to be: %lf\n", max);
+    XArray arr = nc.zeros(SHAPE(TEST_ARR_LEN, 1, 1, 1), INT);
 
     return 0;
 }
